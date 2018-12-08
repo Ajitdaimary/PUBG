a7f4aad8ac1ef5d57769e740ca39676823fa5c05
Marcos <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-08 10:35:23 +0530

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -103,14 +103,14 @@ async def avatar(ctx, user: discord.Member=None):
         r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
         embed = discord.Embed(title=f'Avatar', description='Avatar is profile picture of a user in discord', color = discord.Color((r << 16) + (g << 8) + b))
         embed.add_field(name='User: {}'.format(ctx.message.author.name), value='Avatar:', inline=True)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/icons/489333893988745217/eb022a7023d013bec656cd7b94d6d6c1.png') 
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/520159870448566287/520827266612330506/70abfd2357d07d909dac202e8d1ac32d--dark-grey-hair-ulzzang-makeup.jpg') 
         embed.set_image(url = ctx.message.author.avatar_url)
         await client.say(embed=embed)
     else:
         r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
         embed = discord.Embed(title=f'Avatar', description='Avatar is profile picture of a user in discord', color = discord.Color((r << 16) + (g << 8) + b))
         embed.add_field(name='User: {}'.format(user.name), value='Avatar:', inline=True)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/icons/489333893988745217/eb022a7023d013bec656cd7b94d6d6c1.png') 
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/520159870448566287/520827266612330506/70abfd2357d07d909dac202e8d1ac32d--dark-grey-hair-ulzzang-makeup.jpg') 
         embed.set_image(url = user.avatar_url)
         await client.say(embed=embed)
 
