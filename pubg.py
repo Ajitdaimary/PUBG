bd942a77541bf1561e09653b0fe3c054df33fea4
Marcos <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-09 11:03:10 +0530

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -10,6 +10,21 @@ import os
 import functools
 import time
 import datetime
+import json
+import textwrap
+#from .cogs.utils.utils import Utils
+from .AudioNode import AudioNode
+from .AudioPlayer import AudioPlayer
+from .Events import TrackStart
+import websockets
+from pyee import EventEmitter
+from .Events import TrackEnd
+#from utils.Logger import Logge
+from .Events import TrackStart, QueueConcluded
+from .AudioTrack import AudioTrack
+from discord import User
+
+
 
 client = commands.Bot(description="Here is some command for you", command_prefix=commands.when_mentioned_or("p!"), pm_help = False)
 
@@ -291,5 +306,326 @@ async def membercount(ctx, *args):
 
     await client.send_message(ctx.message.channel, embed=em)
     await client.delete_message(ctx.message)
+	
+class AudioManager:
+    """
+    Class of the AudioManager section.
+    This is main class and controls all stuff like joining channels, leaving channels, and launching nodes.
+    """
+    def __init__(self, bot, nodes, shards=1):
+        self.bot = bot
+        bot.add_listener(self.on_socket_response)
+        self.nodes = {}
+        self.players = {}
+        self._nodes = nodes
+        self.shards = shards
+        self.session = self.bot.session
+        self.utils = self.bot.utils
+
+    def get_player(self, ctx):
+        player = self.players.get(ctx.guild.id)
+        if player is None:
+            player = AudioPlayer(ctx, self, self.nodes.get(self._nodes[0]["host"]))
+            self.players[ctx.guild.id] = player
+
+        return player
+
+    async def get_tracks(self, player, search: str):
+        async with self.session.get(f"http://{player.node.host}:2333/loadtracks?identifier={search}", headers={"Authorization": player.node.password}) as resp:
+            tracks = await resp.json()
+            return tracks
+    
+    async def on_socket_response(self, data):
+        if data["t"] == "VOICE_SERVER_UPDATE":
+            payload = {
+                "op": "voiceUpdate",
+                "guildId": data["d"]["guild_id"],
+                "sessionId": self.bot.get_guild(int(data["d"]["guild_id"])).me.voice.session_id,
+                "event": data["d"]
+            }
+            await self.nodes.get(self._nodes[0]["host"]).send(**payload)
+
+    async def connect(self, ctx):
+        await self.bot.ws.send(json.dumps({
+            "op": 4,
+            "d": {
+                "guild_id": ctx.guild.id,
+                "channel_id": ctx.author.voice.channel.id,
+                "self_mute": False,
+                "self_deaf": False
+            }
+        }))
+        self.get_player(ctx).is_connected = True
+
+    async def leave(self, ctx):
+        await self.bot.ws.send(json.dumps({
+            "op": 4,
+            "d": {
+                "guild_id": ctx.guild.id,
+                "channel_id":  None,
+                "self_mute": False,
+                "self_deaf": False
+            }
+        }))
+        try:
+            del self.players[ctx.guild.id]
+        except KeyError:
+            pass
+        
+    async def audio_task(self):
+        for i in range(len(self._nodes)):
+            node = AudioNode(self, self.shards, self._nodes[i]["host"], self._nodes[i]["password"], self._nodes[i]["port"])
+            await node.launch()
+            self.nodes[node.host] = node
+        self.bot.loop.create_task(self.node_event_task())
+
+    async def node_event_task(self):
+
+        for node in self.nodes.values():
+            @node.ee.on("track_start")
+            async def on_track_start(e):
+                print("Music: track_start event triggered.")
+                ctx = e.player.ctx
+                #print(dir(e))
+                f = e
+                e = e.track
+                #print(dir(e))
+                em = discord.Embed(color=0x00ff00, title=f"Music")
+                #em.description = f"**{e.track.title}**"
+                em.set_author(name=e.requester.name, icon_url=e.requester.avatar_url)
+                second = e.length / 1000
+                minute, second = divmod(second, 60)
+                hour, minute = divmod(minute, 60)
+                #minutes, seconds = divmod(e.track.duration, 60)
+                #em.add_field(name='Length', value=f"{str(minutes)}:{str(seconds).replace('0', '00').replace('1', '01').replace('2', '02').replace('3', '03').replace('4', '04').replace('5', '05').replace('6', '06').replace('7', '07').replace('8', '08').replace('9', '09')}")
+                if hour:
+                    length = f"{int(hour)}:{self.utils.format_time(minute)}:{self.utils.format_time(second)}"
+                else:
+                    length = f"{self.utils.format_time(minute)}:{self.utils.format_time(second)}"
+                playing_panel = textwrap.dedent(f"""
+I started playing the music! {self.bot.get_emoji(511089456196091916)}
+:musical_note: **Song**
+{e.title}
+{self.bot.get_emoji(430340802879946773)} **Requested By**
+{str(ctx.author)}
+:timer: **Length**
+{length}
+:loud_sound: **Volume**
+{f.player.volume}
+:1234: **Queue Position**
+{len(f.player.queue)}
+                """)
+                #em.add_field(name='Length', value=length)
+                #em.add_field(name='Volume', value=f"{self.utils.get_lines(e.player.volume)} {e.player.volume}%")
+                em.description = playing_panel
+                #em.add_field(name='Position in Queue', value=len(e.player.queue))
+                msg = await ctx.send(embed=em, edit=False)
+                try:
+                    await msg.add_reaction("\U000023f8") # Pause
+                    await msg.add_reaction("\U000025b6") # Play/Resume
+                    await msg.add_reaction("\U000023f9") # Stop
+                    await msg.add_reaction("\U0001f501") # Repeat
+                    await msg.add_reaction("\U00002753") # Help
+                except discord.Forbidden:
+                    return await ctx.send("I don't have Add Reaction permissions, so I can't show my awesome playing panel!")
+                try:    
+                    while f.player.playing:
+                        if len(ctx.author.voice.channel.members) <= 1:
+                            return await ctx.send(f"Guys? Seriously? Well, guess I'm out too. {self.bot.get_emoji(517142988904726562)}")
+                        reaction, user = await self.bot.wait_for("reaction_add", check=lambda r, u: u.id == ctx.author.id and r.emoji in "⏸▶⏹🔁❓")
+                        if reaction.emoji == "⏸":
+                            await e.pause()
+                            try:
+                                await msg.remove_reaction("\U000023f8", user)
+                            except: 
+                                pass
+                        elif reaction.emoji == "▶":
+                            await e.resume()
+                            await msg.remove_reaction("\U000025b6", user)
+                        elif reaction.emoji == "⏹":
+                            e.player.queue.clear()
+                            await e.stop()
+                            await msg.delete()
+                        elif reaction.emoji == "🔁":
+                            e.repeating = not e.repeating
+                            await msg.remove_reaction("\U0001f501", user)
+                        elif reaction.emoji == "❓":
+                            await msg.remove_reaction("\U00002753", user)
+                            embed = discord.Embed(color=0x00ff00, title='Music Player Help')
+                            embed.description = "**What do these magical buttons do?** \n\n:pause_button: Pauses the current song.\n:arrow_forward: Resumes any currently paused song.\n:stop_button: Stops the playing song and deletes this message.\n:repeat: Starts the current song from the beginning.\n:question: Shows this message."
+                            embed.set_footer(text='This will revert back in 15 seconds.')
+                            await msg.edit(embed=embed)
+                            await asyncio.sleep(15)
+                            await msg.edit(embed=em)
+                except discord.Forbidden:
+                    pass # No need to send 
+                # except Exception as e:
+                #     return await ctx.send(f"An unknown error occured. Details: \n\n```{e}```")
+
+                # This made shit way too spammy, can't think of a good way to avoid it, rather just remove it.
+
+            @node.ee.on("track_end")
+            async def on_track_end(event):
+                print("Music: track_end event triggered.")
+                if event.reason == "REPLACED":
+                    return  # Return because if we play then the queue will be fucked.
+                elif event.reason == "FINISHED":
+                    if event.player.repeating:
+                        await event.player.node.send(op="play", guildId=str(event.player.ctx.guild.id), track=event.player.current.track)
+                        return event.player.node.ee.emit("track_start", TrackStart(event.player, event.player.current))
+                    await event.player.play()
+
+            @node.ee.on("queue_concluded")
+            async def on_queue_concluded(event):
+                print("Music: queue_concluded event triggered.")
+                await self.leave(event.player.ctx)
+		
+class AudioNode:
+    def __init__(self, manager, shards, host=None, password=None, port=None):
+        self.stats = {}
+        self.ee = EventEmitter()
+        self._manager = manager
+        self.shards = shards
+        self.ready = False
+        self.ws = None
+        self.host = host
+        self.password = password
+        self.port = port
+        self.stats = None
+        self.reconnect_tries = 0
+
+    def __str__(self):
+        return f"""
+A lavalink node class for the lavalink manager.
+Shards: {self.shards}
+Host: {self.host}
+Port: {self.port}"""
+
+    async def _wait_for_ws_message(self):
+        while self.ws.open:
+            try:
+                data = json.loads(await self.ws.recv())
+            except websockets.ConnectionClosed:
+                return self._manager.bot.loop.create_task(self.launch())
+
+            if data["op"] == "playerUpdate":
+                player = self._manager.players.get(int(data["guildId"]))
+                if player:
+                    player.state["timestamp"] = data["state"]["time"]
+                    player.state["position"] = data["state"]["position"]
+            elif data["op"] == "stats":
+                del data["op"]
+                self.stats = data
+            elif data["op"] == "event":
+                player = self._manager.players.get(int(data["guildId"]))
+                if data["type"] == "TrackEndEvent":
+                    if player:
+                        self.ee.emit("track_end", TrackEnd(player, data["track"], data["reason"]))
+
+    def _headers(self):
+        return {
+            "Authorization": self.password,
+            "Num-Shards": self.shards,
+            "User-Id": self._manager.bot.user.id
+        }
+
+    async def launch(self):
+        try:
+            self.ws = await websockets.connect(f"ws://{self.host}:{self.port}", extra_headers=self._headers())
+            if self.ws.open:
+                self._manager.bot.loop.create_task(self._wait_for_ws_message())
+                self.ready = True
+        except OSError as error:
+            pass
+
+    async def send(self, **data):
+        if self.ws.open:
+            await self.ws.send(json.dumps(data))
+	
+	
+class AudioPlayer:
+    """
+    Class of AudioPlayer.
+    This class has many uses.
+    It can play music, skip music, pause music, resume music, seek music, and much more.
+    """
+    def __init__(self, ctx, manager, node):
+        self.ctx = ctx
+        self.state = {}
+        self.manager = manager
+        self.node = node
+        self.volume = 50
+        self.playing = False
+        self.paused = False
+        self.repeating = False
+        self.current = None
+        self.is_connected = False
+        self.queue = []
+        self.m = None
+
+    def enqueue(self, track: dict, requester: User):
+        self.queue.append(AudioTrack().make(track, requester))
+
+    async def play(self):
+        if not self.queue:
+            self.node.ee.emit("queue_concluded", QueueConcluded(self.manager.get_player(self.ctx)))
+        else:
+            self.playing = True
+            track = self.queue.pop(0)
+            self.current = track
+            await self.node.send(op="play", guildId=str(self.ctx.guild.id), track=track.track)
+            self.node.ee.emit("track_start", TrackStart(self.manager.get_player(self.ctx), track))
+
+    async def stop(self):
+        await self.node.send(op="stop", guildId=str(self.ctx.guild.id))
+
+    async def set_paused(self, paused):
+        self.paused = paused
+        await self.node.send(op="pause", guildId=str(self.ctx.guild.id), pause=self.paused)
+
+    @staticmethod
+    def valid_volume(volume):
+        return 10 <= volume <= 150
+
+    async def set_volume(self, volume):
+        if not self.valid_volume(volume):
+            return
+        self.volume = volume
+        await self.node.send(op="volume", guildId=str(self.ctx.guild.id), volume=volume)
+	
+class AudioTrack:
+    def make(self, track, requester):
+        try:
+            self.track = track["track"]
+            self.id = track["info"]["identifier"]
+            self.seekable = track["info"]["isSeekable"]
+            self.author = track["info"]["author"]
+            self.length = track["info"]["length"]
+            self.stream = track["info"]["isStream"]
+            self.title = track["info"]["title"]
+            self.url = track["info"]["uri"]
+            self.requester = requester
+
+            return self
+        except KeyError:
+            raise Exception("Invalid track passed: {}".format(str(track)))
+
+	
+class TrackStart:
+    def __init__(self, player, track):
+        self.player = player
+        self.track = track
+
+
+class TrackEnd:
+    def __init__(self, player, track, reason):
+        self.player = player
+        self.track = track
+        self.reason = reason
+
+
+class QueueConcluded:
+    def __init__(self, player):
+        self.player = player	
 	
 client.run(os.getenv('Token'))
