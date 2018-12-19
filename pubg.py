0ed20252d567f293f5c087f8977f42c5daf28890
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-19 15:17:33 +0530

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -70,6 +70,21 @@ async def ping(ctx):
     await client.send_typing(channel)
     t2 = time.perf_counter()
     await client.say("Ping: {}ms".format(round((t2-t1)*1000)))
-	
+
+
+@client.event
+async def on_member_join(member):
+    for channel in member.server.channels:
+        if channel.name == '★彡-welcome-彡★':
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+            embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
+            embed.add_field(name='Your join position is', value=member.joined_at)
+            embed.set_thumbnail(url=member.avatar_url)
+            await client.send_message(channel, embed=embed)
+
+
+
+
 		
 client.run(os.getenv('Token'))
